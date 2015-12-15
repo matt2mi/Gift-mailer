afb14cc24d4b00c6342836ea18ea9155a9db0ec0
mdeumie <2m1tema@gmail.com>
2015-12-15 16:32:15 +0100

    maj

--- server.js
+++ server.js
@@ -6,12 +6,15 @@ var path = require('path');
 
 var app = express();
 app.set('port', (process.env.PORT || 5000));
+
+app.set('public', __dirname + '/public');
+
 app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
 app.use(bodyParser.json());                                     // parse application/json
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 
 app.get('/', function(req, res, next) {
-    res.sendFile('public/index.html');
+    res.sendFile('index.html');
 });
 
 app.use(function(req, res) {
